08012d72fc82600fb88944ba932cb14895ba4592
Petter Arvidsson <petter.arvidsson@fidesmo.com>
2014-10-31 11:56:10 +0100

    Add support for multiple AIDs
    - Add support for providing multiple AIDs
    - Add list of default AIDs (Fidesmo and Yubico)

--- src/main/java/com/fidesmo/oath/hardware/HardwareToken.java
+++ src/main/java/com/fidesmo/oath/hardware/HardwareToken.java
@@ -38,18 +38,31 @@ import java.util.List;
  */
 public interface HardwareToken {
 
-    /** Open must be called before any operation is executed, returns the unique id of the token
+    /** Open must be called before any operation is executed, returns the AID selected
+     *  This attempts to select the default AIDs.
      */
     public byte[] open() throws IOException;
 
-    /** Close must be called befor the object is destroyed. After closing no fruther operations shall be executed
+    /** Open must be called before any operation is executed, returns the AID selected
+     *  @param aids A list of AIDs to be attempted
+     */
+    public byte[] open(List<byte[]> aids) throws IOException;
+
+    /** Close must be called before the object is destroyed
+     *  After closing no fruther operations shall be executed
      */
     public void close() throws IOException;
 
+    /** Return ID of cardlet
+     */
+    public byte[] getId();
+
     /** Store a secret key for TOTP or HOTP on the hardware token
      *
-     * @param digits maybe the number of digits (default is 6)
-     * @param counterOrInterval if algorithm is HOTP must be the initial counter if algorithm is TOTP  might be lifeperiod of a single code in seconds (default is 30)
+     *  @param digits the number of digits (default is 6)
+     *  @param counterOrInterval if algorithm is HOTP must be the initial
+     *                           counter if algorithm is TOTP  might be lifeperiod
+     *                           of a single code in seconds (default is 30)
      */
     public void storeCode(TokenMeta token, byte[] key, int counterOrPeriod) throws IOException;
 
